3e292609c3d3da0157ee5f8f288259dc10fbabeb
mariamezzat01 <mariammohamedezzat2010@gmail.com>
2024-01-03 17:36:44 +0200

    ..

---
+++ trial.py
@@ -0,0 +1,13 @@
+from df.enhance import enhance, init_df, load_audio, save_audio
+from df.utils import download_file
+
+if __name__ == "__main__":
+    # Load default model
+    model, df_state, _ = init_df()
+    # Download and open some audio file. You use your audio files here
+    audio_path = 'noise1.wav'
+    audio, _ = load_audio(audio_path, sr=df_state.sr())
+    # Denoise the audio
+    enhanced = enhance(model, df_state, audio)
+    # Save for listening
+    save_audio("noise1_phase1.wav", enhanced, df_state.sr())
